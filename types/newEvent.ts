968c8cb87f0a8c52876e0045988bf541a3197eca
keigo <2364138keigo@gmail.com>
2026-06-08 09:40:23 +0900

    post用のデータの型を作成

---
+++ types/newEvent.ts
@@ -0,0 +1,11 @@
+export type newEventType = {
+  summary: string | null;
+  start: {
+    date: string | null;
+    dateTime: string | null;
+  };
+  end: {
+    date: string | null;
+    dateTime: string | null;
+  };
+};
